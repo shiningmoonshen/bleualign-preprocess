a0244e3aaa3d1f05b4a01f8813e6af8f0930a4e5
Esther Shen <38596783+shiningmoonshen@users.noreply.github.com>
2023-01-03 20:46:50 -0800

    fixed bug where period of last sentence on a line would be ommitted

--- sentence_split_ch.py
+++ sentence_split_ch.py
@@ -4,12 +4,13 @@ import sys
 
 def main():
     #opens source file and rewrites file with each sentence having a new line
-    punctuation = re.compile(r"([^\d+])(\.|\!|\?|;|\n|。|\！|\？|；|…)+")
+    punctuation = re.compile(r"([^\d+])(\.|\!|\?|;|。\n|\n|。|\！|\？|；|…)+")
     lines = []
 
     with open(sys.argv[1],'r',encoding="utf-8") as f:
         lines = punctuation.sub(r"\1\2<pad>", f.read())
         lines = [line.strip() for line in lines.split("<pad>") if line.strip()]
+
 
     new = open(sys.argv[1][:-4] + "_output.txt", "w")
     for line in lines:
